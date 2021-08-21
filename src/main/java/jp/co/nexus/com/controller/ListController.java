d2f00dca174a6349128b0c3dce3a798c6aaf52a0
yamazaki-shugo <yamaz1209876@icloud.com>
2021-08-21 14:53:31 +0900

    漫画一覧取得しHTMLに保存

--- src/main/java/jp/co/nexus/com/controller/ListController.java
+++ src/main/java/jp/co/nexus/com/controller/ListController.java
@@ -1,10 +1,13 @@
 package jp.co.nexus.com.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,15 +28,25 @@ public class ListController {
 	ListService listService;
 
 	@GetMapping("/list")
-	public String comicList() {
+	public String comicList(Model model) {
 
 		// 画面遷移先を漫画一覧画面に指定
 		String res = "com/list";
+		
+		// 50音の配列を定義
+		List<String> syllabary = new ArrayList<String>(Arrays.asList("ア", "イ", "ウ", "エ", "オ",
+				"カ", "キ", "ク", "ケ", "コ", "サ", "シ", "ス", "セ", "ソ", "タ", "チ", "ツ", "テ", "ト",
+				"ナ", "ニ", "ヌ", "ネ", "ノ", "ハ", "ヒ", "フ", "ヘ", "ホ", "マ", "ミ", "ム", "メ", "モ",
+				"ヤ", "ユ", "ヨ", "ラ", "リ", "ル", "レ", "ロ", "ワ", "ヲ", "ン"));
 
 		// DBより漫画一覧をアイウエオ順に取得
 		List<List<Map<String, Object>>> l_list = listService.searchRuby();
+
+		// 取得したリストデータを保存
+		model.addAttribute("l_list", l_list);
 		
-		System.out.println(l_list);
+		// 50音の配列データを保存
+		model.addAttribute("syllabary", syllabary);
 
 		return res;
 	}
